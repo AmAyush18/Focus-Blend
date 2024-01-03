419ca86f12a76548529f328aa9a4bc41c7814195
Ayush Dwivedi <amayush0818@gmail.com>
2024-01-03 05:33:55 +0530

    Completed Donation Page

--- src/components/Donate.jsx
+++ src/components/Donate.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import buymeacoffee from '../assets/bmc_qr.png';
+import buymeacoffee_button from '../assets/bmc-button.png';
 
 function Donate() {
   return (
-    <div>Donate</div>
+    <div className="container w-[70%] mx-auto mt-8 p-4">
+      <h2 className="text-2xl font-bold mb-4 text-center">Support Serene</h2>
+      
+      <p className="text-gray-700 leading-loose mb-4 text-center">
+        Serene is a labor of love. If you find it helpful, consider supporting us by buying us a coffee. Your contribution goes a long way in keeping Serene free and continuously improving.
+      </p>
+
+      <div className="flex items-center justify-center">
+        <Link to="https://www.buymeacoffee.com/amayush18" target="_blank" rel="noopener noreferrer">
+          <img src={buymeacoffee_button} alt="" className='w-[400px] h-[78px]' />
+        </Link>
+      </div>
+      
+      <div className="flex items-center justify-center mt-5">
+        <img src={buymeacoffee} alt="" className='w-[420px] h-[420px]' />
+      </div>
+    </div>
   )
 }
 
